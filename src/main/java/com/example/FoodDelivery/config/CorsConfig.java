63e1f9f8cb4518947a4d57c9fcf5cceec4594831
Duong Hieu <hieuduong181005@gmail.com>
2025-12-04 16:30:45 +0700

    fix cors

--- src/main/java/com/example/FoodDelivery/config/CorsConfig.java
+++ src/main/java/com/example/FoodDelivery/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(
                 Arrays.asList("http://localhost:3000", "http://localhost:4173", "http://localhost:5173",
                         "https://eatzy-frontend-restaurant.vercel.app", "https://eatzy-frontend-customer.vercel.app",
-                        "https://eatzy-frontend-driver.vercel.app")); // Allowed origins
+                        "https://eatzy-frontend-driver.vercel.app", "https://eatzy_be.hoanduong.net/")); // Allowed
+                                                                                                         // origins
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
         configuration.setAllowCredentials(true);
